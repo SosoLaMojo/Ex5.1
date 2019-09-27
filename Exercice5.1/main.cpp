03e7778ba151c8aa5790461c0d189912b73fded2
SosoLaMojo <sol.sch1990@gmail.com>
2019-09-27 13:36:28 +0200

    Ajout fonction GenerateMysteryNumber

--- Exercice5.1/main.cpp
+++ Exercice5.1/main.cpp
@@ -1,4 +1,17 @@
 #include <iostream>
+#include <time.h>
+
+/*EXERCICE 5.1
+Rendu Git
+J’ai créer un repository qui s’appel Ex5.1(https://github.com/Surue/Ex5.1) que vous devez le fork chez vous.
+Le lancer et faire en sorte que celui-ci compile et compléter les parties manquantes indiquée dans le fichier main.cpp.
+Toutes ses modifications doivent être faite sur une nouvelle branche créé à partir de develop que vous appelez comme vous le voulez.
+Vous faites un commit pour chaque fonction complétée. Une fois l’exercices terminé vous devez merge les modifications de votre branch sur develop.
+*/
+
+
+
+
 
 enum class GameState {
 	INIT,
@@ -7,30 +20,41 @@ enum class GameState {
 	EXIT
 };
 
-void GenerateMysteryNumber(int& mystery) {
+void GenerateMysteryNumber(unsigned int& mystery) {
 	
+	srand(time(NULL));
+
+	int MAX_NUMBER = 101;
+	
+	mystery = (rand() % MAX_NUMBER);
+
 }
 
 int main() {
+
 	GameState gameState = GameState::INIT;
 
 	unsigned int mysteryNumber;
 
 	while (gameState != GameState::EXIT);
 	{
+
+
 		switch (gameState) {
 		case GameState::INIT:
-			/*
-				Cette partie doit donner une valeur à mysterNumber comprise entre 0 et 100
-			*/
+			/*Cette partie doit donner une valeur à mysterNumber comprise entre 0 et 100*/
+			
+			GenerateMysteryNumber(mysteryNumber);
 			gameState = GameState::PLAY;
+
+			//std::cout << "le nombre mystere choisit est: " << mysteryNumber;
 			break;
 
 		case GameState::PLAY:
 			/*
 				Cette partie doit:
-				- essayer de deviner le nombre mystère à l'aide d'un function
-				- Si le nombre mystre est trouvé il faut passer à l'état END
+				- essayer de deviner le nombre mystère à l'aide d'une function
+				- Si le nombre mystère est trouvé il faut passer à l'état END
 			*/
 			break;
 
@@ -43,9 +67,9 @@ int main() {
 		case GameState::EXIT:
 			break;
 		}
+		
 	}
-
 	
-	system("pose");
-	return EXIT_FAILURE;
+system("pause");
+return EXIT_SUCCESS;
 }
